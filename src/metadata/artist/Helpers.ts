811b4c2037edaad03472ef004726967a66fb8ddd
enchart <77974051+enchart@users.noreply.github.com>
2022-05-06 20:47:11 +0500

    Added doc for CreateArtist()

--- src/metadata/artist/Helpers.ts
+++ src/metadata/artist/Helpers.ts
@@ -1,6 +1,11 @@
 import { Artist } from "./Artist";
 import { ArtistConstructor } from "./ArtistConstructor";
 
+/**
+ * Constructs a new Artist.
+ * @param constructor A Artist constructor object.
+ * @returns A new Artist.
+ */
 export function CreateArtist(constructor: ArtistConstructor): Artist {
   return new Artist(constructor);
 }
